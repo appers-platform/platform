b7e6933b856270d9fef564bdc5ccde618c87a4be
Vlad <vlad.amardi@gmail.com>
2015-03-27 20:24:10 +0200

    update sample apper

---
+++ data/sample-apper.dev/config/parent.php
@@ -0,0 +1,6 @@
+<? return [
+	'mysql'		=> [ 'db' 		=> 'sample-apper_dev' ],
+	'memcache'	=> [ 'prefix'	=> 'sample-apper_dev' ],
+	'gearman'	=> [ 'prefix'	=> 'sample-apper_dev' ],
+	'secret'	=> 'd873cecojsU79so879KHihsikk',
+];
