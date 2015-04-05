4a32fb2d9c3adb9c1eec1ba89e92abb9bf96d6f3
Vlad <vlad.amardi@gmail.com>
2015-04-05 15:59:50 +0300

    Solution 'user'

--- solutions/user/controller/confirm.php
+++ solutions/user/controller/confirm.php
@@ -10,11 +10,11 @@ $confirm = \helper::decode(
 if($model = userModel::getByEmail($confirm)) {
 	$url = \solutions\user::getAfterAuthUrl();
 	
-	log::debug('model:'.print_r($model, true));
-
 	if(!$model->is_confirmed) {
 		$model->is_confirmed = 1;
+		log::debug('model:'.print_r($model, true));
 		$model->save();
+		log::debug('model:'.print_r($model, true));
 
 		$after_auth_model = new userAuthModel();
 		$after_auth_model->user_id = $model->getId();
